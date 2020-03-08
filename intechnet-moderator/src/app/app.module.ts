a4a9b80db848fae465d19b8d93d4887bd4f1cfb2
pBouillon <pro.pierre.bouillon@protonmail.com>
2020-03-08 22:56:22 +0100

    🌱 WiP homepage presentation

--- intechnet-moderator/src/app/app.module.ts
+++ intechnet-moderator/src/app/app.module.ts
@@ -4,13 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { GlobalModule } from './global/global.module';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    GlobalModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- intechnet-moderator/src/app/global/global.module.ts
+++ intechnet-moderator/src/app/global/global.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomepageComponent } from './homepage/homepage.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
     HomepageComponent,
+    NavbarComponent,
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule,
   ]
 })
 export class GlobalModule { }
