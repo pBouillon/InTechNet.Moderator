466cb97ff248f168321a98bead769167f1eb66e9
pBouillon <pro.pierre.bouillon@protonmail.com>
2020-03-14 22:08:59 +0100

    🚧 Raw authentication service

---
+++ intechnet-moderator/src/app/_models/user.ts
@@ -0,0 +1,7 @@
+export class User {
+    email: string;
+    id: number;
+    nickname: string;
+    password: string;
+    token: string;
+}
---
+++ intechnet-moderator/src/app/_services/authentication.service.ts
@@ -0,0 +1,58 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+import { User } from '../_models/user';
+import { LocalStorageService } from './local-storage.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthenticationService {
+
+  private currentUserSubject: BehaviorSubject<User>;
+  public currentUser: Observable<User>;
+
+  constructor(
+    private http: HttpClient,
+    private storageService: LocalStorageService,
+  ) {
+    this.currentUserSubject = new BehaviorSubject<User>(JSON.parse(localStorage.getItem('currentUser')));
+    this.currentUser = this.currentUserSubject.asObservable();
+  }
+
+  public get currentUserValue(): User {
+    return this.currentUserSubject.value;
+  }
+
+  moderatorLogin(login: string, password: string) {
+    return this.http.post<any>(`/api/v1/Moderator/login`, { login, password })
+      .pipe(map(user => {
+        if (user && user.token) {
+          this.storageService.store('currentUser', JSON.stringify(user));
+          this.currentUserSubject.next(user);
+        }
+
+        return user;
+      }));
+  }
+
+  pupilLogin(login: string, password: string) {
+    return this.http.post<any>(`/api/v1/Pupil/login`, { login, password })
+      .pipe(map(user => {
+        if (user && user.token) {
+          this.storageService.store('currentUser', JSON.stringify(user));
+          this.currentUserSubject.next(user);
+        }
+
+        return user;
+      }));
+  }
+
+  logout() {
+    this.storageService.clear('currentUser');
+    this.currentUserSubject.next(null);
+  }
+
+}
