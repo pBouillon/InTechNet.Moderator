05551380adee42fb5daa138a890b32368c4faf8f
pBouillon <pro.pierre.bouillon@protonmail.com>
2020-03-19 09:32:53 +0100

    ✨ Add contact page

--- intechnet-moderator/src/app/homepage/contact/contact.component.ts
+++ intechnet-moderator/src/app/homepage/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-contact',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactComponent implements OnInit {
 
+  /**
+   * @summary fetch the current environment contact address
+   */
+  mailAddress = environment.mailAddress;
+
   constructor() { }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void { }
 
 }
