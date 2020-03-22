819730da2ce62571cef70ad1ea17edcf0be4d7b3
Raphael Clarisse <raphael.clarisse21@gmail.com>
2020-03-22 20:16:44 +0100

    ✅ Add tests for register component

--- intechnet-moderator/src/app/authentication/register/register.component.spec.ts
+++ intechnet-moderator/src/app/authentication/register/register.component.spec.ts
@@ -1,6 +1,9 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { RegisterComponent } from './register.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { FormBuilder } from '@angular/forms';
 
 describe('RegisterComponent', () => {
   let component: RegisterComponent;
@@ -8,6 +11,11 @@ describe('RegisterComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule,
+        HttpClientTestingModule
+       ],
+      providers: [ FormBuilder],
       declarations: [ RegisterComponent ]
     })
     .compileComponents();
