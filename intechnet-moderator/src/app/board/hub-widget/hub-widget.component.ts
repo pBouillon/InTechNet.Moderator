0c32d6de1a2ef6849e3d8c83adeece0c5b1f5a84
pBouillon <pro.pierre.bouillon@protonmail.com>
2020-03-23 13:59:45 +0100

    ✨ Add link copy to clipboard

--- intechnet-moderator/src/app/board/hub-widget/hub-widget.component.ts
+++ intechnet-moderator/src/app/board/hub-widget/hub-widget.component.ts
@@ -26,8 +26,8 @@ export class HubWidgetComponent implements OnInit, AfterViewInit {
   /**
    * @summary Emit the id of the hub on user's deletion request
    */
-  @Output('hubDeletionEvent')
-  idToBeDeletedEvent: EventEmitter<number> = new EventEmitter<number>();
+  @Output()
+  hubDeletionEvent: EventEmitter<number> = new EventEmitter<number>();
 
   constructor(
     private hubService: HubService,
@@ -41,10 +41,36 @@ export class HubWidgetComponent implements OnInit, AfterViewInit {
   ngOnInit(): void { }
 
   /**
+   * @summary copy the hub's shareable link to the user's clipboard
+   */
+  onCopyShareableLink(): void {
+    // Create temporary component holding the text to be copied
+    const tempTextBox = document.createElement('textarea');
+    tempTextBox.style.position = 'fixed';
+    tempTextBox.style.left = '0';
+    tempTextBox.style.top = '0';
+    tempTextBox.style.opacity = '0';
+
+    // Put the shareable link in the text box
+    tempTextBox.value = this.lightweightHub.link;
+
+    // Add the text box to the DOM
+    document.body.appendChild(tempTextBox);
+
+    // Focus and copy the content of the text box
+    tempTextBox.focus();
+    tempTextBox.select();
+    document.execCommand('copy');
+
+    // Clean DOM by removing the component
+    document.body.removeChild(tempTextBox);
+  }
+
+  /**
    * @summary delete the current hub
    */
   onDeleteRequest(): void {
-    this.idToBeDeletedEvent.emit(this.lightweightHub.id);
+    this.hubDeletionEvent.emit(this.lightweightHub.id);
   }
 
   /**
