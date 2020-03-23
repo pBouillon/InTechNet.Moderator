172cba5d04d70c5332297787d775dbf6e7f26bc8
Pierre Bouillon <pro.pierre.bouillon@protonmail.com>
2020-03-23 13:42:01 +0100

    ✨ Deletion modal (#22)
    * 🚧 Deletion modal
    
    * Add hub deletion confirmation modal
    
    * Modal toggle

--- intechnet-moderator/src/app/board/hub-widget/hub-widget.component.ts
+++ intechnet-moderator/src/app/board/hub-widget/hub-widget.component.ts
@@ -1,7 +1,12 @@
-import { Component, OnInit, Input, AfterViewInit } from '@angular/core';
+import { Component, OnInit, Output, Input, AfterViewInit, EventEmitter } from '@angular/core';
 import { LightweightHub } from 'src/app/_models/entities/hub/lightweight-hub';
 
 import * as feather from 'feather-icons';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { HubService } from 'src/app/_services/hub/hub.service';
+import { HttpErrorResponse } from '@angular/common/http';
+import { RouteName } from 'src/app/routing/route-names';
 
 @Component({
   selector: 'app-hub-widget',
@@ -10,19 +15,37 @@ import * as feather from 'feather-icons';
 })
 export class HubWidgetComponent implements OnInit, AfterViewInit {
 
+  deletionModalName: string;
+
   /**
    * @summary Content to display
    */
   @Input()
   public lightweightHub: LightweightHub;
 
-  constructor() { }
+  /**
+   * @summary Emit the id of the hub on user's deletion request
+   */
+  @Output('hubDeletionEvent')
+  idToBeDeletedEvent: EventEmitter<number> = new EventEmitter<number>();
+
+  constructor(
+    private hubService: HubService,
+    private router: Router,
+  ) { }
 
   ngAfterViewInit(): void {
     this.useFeatherIcons();
   }
 
   ngOnInit(): void { }
+
+  /**
+   * @summary delete the current hub
+   */
+  onDeleteRequest(): void {
+    this.idToBeDeletedEvent.emit(this.lightweightHub.id);
+  }
 
   /**
    * @summary Replace the feather icons tag by svg source
