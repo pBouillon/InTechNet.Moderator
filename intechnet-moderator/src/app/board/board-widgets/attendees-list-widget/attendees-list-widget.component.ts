67ccb7a08127cfab2f23676a0a460746d063adab
pBouillon <pro.pierre.bouillon@protonmail.com>
2020-03-25 10:49:13 +0100

    🌱 Create hub details widgets

---
+++ intechnet-moderator/src/app/board/board-widgets/attendees-list-widget/attendees-list-widget.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-attendees-list-widget',
+  templateUrl: './attendees-list-widget.component.html',
+  styleUrls: ['./attendees-list-widget.component.scss']
+})
+export class AttendeesListWidgetComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- intechnet-moderator/src/app/board/board.module.ts
+++ intechnet-moderator/src/app/board/board.module.ts
@@ -5,12 +5,18 @@ import { HubWidgetComponent } from './hub-widget/hub-widget.component';
 import { NewHubComponent } from './new-hub/new-hub.component';
 import { RouterModule } from '@angular/router';
 import { HubDetailsComponent } from './hub-details/hub-details.component';
+import { AttendeesListWidgetComponent } from './board-widgets/attendees-list-widget/attendees-list-widget.component';
+import { ModulesListWidgetComponent } from './board-widgets/modules-list-widget/modules-list-widget.component';
+import { HubSettingsWidgetComponent } from './board-widgets/hub-settings-widget/hub-settings-widget.component';
 
 @NgModule({
   declarations: [
     BoardComponent,
     HubWidgetComponent,
     HubDetailsComponent,
+    AttendeesListWidgetComponent,
+    ModulesListWidgetComponent,
+    HubSettingsWidgetComponent,
   ],
   imports: [
     CommonModule,
