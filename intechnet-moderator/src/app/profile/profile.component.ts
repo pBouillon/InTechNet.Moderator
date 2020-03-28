e4e4512a1cdf179d68a032ea392e4603bb10210a
pBouillon <pro.pierre.bouillon@protonmail.com>
2020-03-29 00:10:14 +0100

    🌱 Profile page skeletton

--- intechnet-moderator/src/app/profile/profile.component.ts
+++ intechnet-moderator/src/app/profile/profile.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+
+import * as feather from 'feather-icons';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.scss']
 })
-export class ProfileComponent implements OnInit {
+export class ProfileComponent implements OnInit, AfterViewInit {
 
   constructor() { }
+  
+  ngAfterViewInit(): void {
+    this.useFeatherIcons();
+  }
 
   ngOnInit(): void {
+  }
+
+  /**
+   * @summary Replace the feather icons tag by svg source
+   */
+  private useFeatherIcons(): void {
+    feather.replace();
   }
 
 }
