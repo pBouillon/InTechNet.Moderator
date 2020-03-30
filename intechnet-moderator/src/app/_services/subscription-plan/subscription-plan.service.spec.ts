d14538b781fd18df36f1c8c782d588975aa16e63
pBouillon <pro.pierre.bouillon@protonmail.com>
2020-03-30 20:46:30 +0200

    ✅ Fix unit tests

--- intechnet-moderator/src/app/_services/subscription-plan/subscription-plan.service.spec.ts
+++ intechnet-moderator/src/app/_services/subscription-plan/subscription-plan.service.spec.ts
@@ -1,13 +1,22 @@
 import { TestBed } from '@angular/core/testing';
 
 import { SubscriptionPlanService } from './subscription-plan.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('SubscriptionPlanService', () => {
   let service: SubscriptionPlanService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule,
+      ]
+    });
     service = TestBed.inject(SubscriptionPlanService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
   });
 
   it('should be created', () => {
