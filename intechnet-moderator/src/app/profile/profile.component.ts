c664f30928f856f1a34872ee11ea1be975cac278
pBouillon <pro.pierre.bouillon@protonmail.com>
2020-03-30 20:09:44 +0200

    🚧 Account deletion modal

--- intechnet-moderator/src/app/profile/profile.component.ts
+++ intechnet-moderator/src/app/profile/profile.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit, AfterViewInit } from '@angular/core';
 
 import * as feather from 'feather-icons';
+import { ToastrService } from 'ngx-toastr';
+import { Router } from '@angular/router';
+import { AuthenticationService } from '../_services/authentication/authentication.service';
 
 @Component({
   selector: 'app-profile',
@@ -9,13 +12,37 @@ import * as feather from 'feather-icons';
 })
 export class ProfileComponent implements OnInit, AfterViewInit {
 
-  constructor() { }
-  
+  constructor(
+    private authenticationService: AuthenticationService,
+    private router: Router,
+    private toastr: ToastrService
+  ) { }
+
   ngAfterViewInit(): void {
     this.useFeatherIcons();
   }
 
   ngOnInit(): void {
+  }
+
+  /**
+   * @summary Delete the current account of the logged moderator
+   */
+  public onAccountDeletion(): void {
+    // Delete the profile on the server side
+    this.toastr.warning('Aucun appel à l\'API effectué', 'WiP');
+
+    // logout the current user
+    this.authenticationService.logout();
+
+    // Confirm the account deletion
+    this.toastr.success(
+      'Votre compte a été supprimé avec succès',
+      'Compte supprimé');
+
+    // Redirect to homepage
+    this.router.navigate(['/']);
+    document.getElementById('closeAccountDeletionModal').click();
   }
 
   /**
