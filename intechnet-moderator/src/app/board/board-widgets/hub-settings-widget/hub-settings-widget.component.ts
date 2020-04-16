132e6516619be4173795ae5c4ace6d75be62a343
Raphael Clarisse <raphael.clarisse21@gmail.com>
2020-04-17 00:19:15 +0200

    ✨ Add hub update + 🐛 Fix hub join link case

--- intechnet-moderator/src/app/board/board-widgets/hub-settings-widget/hub-settings-widget.component.ts
+++ intechnet-moderator/src/app/board/board-widgets/hub-settings-widget/hub-settings-widget.component.ts
@@ -5,6 +5,7 @@ import { Hub } from 'src/app/_models/entities/hub/hub';
 import { HttpErrorResponse } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { RouteName } from 'src/app/routing/route-names';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-hub-settings-widget',
@@ -14,18 +15,52 @@ import { RouteName } from 'src/app/routing/route-names';
 export class HubSettingsWidgetComponent implements OnInit {
 
   /**
+   * @summary convenience getter for easy access to form fields
+   */
+  public get f() { return this.updateHubForm.controls; }
+
+  /**
    * @summary Current hub
    */
   @Input()
   public hub: Hub;
 
+  /**
+   * @summary update hub form
+   */
+  updateHubForm: FormGroup;
+
   constructor(
+    private formBuilder: FormBuilder,
     private hubService: HubService,
     private router: Router,
     private toastr: ToastrService
   ) { }
 
-  ngOnInit(): void { }
+  ngOnInit(): void {
+    // Form creation
+    this.createForm();
+  }
+
+  /**
+   * @summary populate the form
+   */
+  private createForm(): void {
+    this.updateHubForm = this.formBuilder.group({
+      name: [
+        '', [
+          Validators.required,
+          Validators.maxLength(22)
+        ]
+      ],
+      description: ['', Validators.maxLength(64)]
+    });
+  }
+
+  isFieldInvalid(field: string) {
+    const formField = this.updateHubForm.get(field);
+    return formField.invalid && (formField.dirty || formField.touched);
+  }
 
   /**
    * @summary copy the hub's shareable link to the user's clipboard
@@ -75,6 +110,26 @@ export class HubSettingsWidgetComponent implements OnInit {
             'Une erreur est survenue lors de la suppression du hub',
             'Erreur de connexion au serveur');
         });
+  }
+
+  /**
+   * @summary Updates the current hub with the given informations
+   */
+  onUpdateHub(): void {
+    const hubDescription = this.f.description.value;
+    const hubName = this.f.name.value;
+
+    this.hubService.updateHub(this.hub.id, hubName, hubDescription)
+      .subscribe(
+        () => {
+          this.toastr.success('Le hub a bien été mis à jour',
+          'Mise à jour effectuée');
+          this.hub.name = hubName;
+          this.hub.description = hubDescription;
+        },
+        () =>
+        this.toastr.error('La mise à jour a échoué',
+          'Erreur lors de la mise à jour'));
   }
 
 }
