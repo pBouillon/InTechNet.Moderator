29ef691a1a76061c4db485c4f81851b9c94e8ab6
pBouillon <pro.pierre.bouillon@protonmail.com>
2020-04-18 15:35:05 +0200

    ✨ Refresh attendees list on hub details

--- intechnet-moderator/src/app/board/hub-details/hub-details.component.ts
+++ intechnet-moderator/src/app/board/hub-details/hub-details.component.ts
@@ -1,24 +1,36 @@
-import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { HubService } from 'src/app/_services/hub/hub.service';
-import { Hub } from 'src/app/_models/entities/hub/hub';
-import { ToastrService } from 'ngx-toastr';
-import { RouteName } from 'src/app/routing/route-names';
 import { HttpErrorResponse } from '@angular/common/http';
 
+import { ToastrService } from 'ngx-toastr';
 import * as feather from 'feather-icons';
+
+import { Hub } from 'src/app/_models/entities/hub/hub';
+import { HubService } from 'src/app/_services/hub/hub.service';
+import { RouteName } from 'src/app/routing/route-names';
+
 
 @Component({
   selector: 'app-hub-details',
   templateUrl: './hub-details.component.html',
   styleUrls: ['./hub-details.component.scss']
 })
-export class HubDetailsComponent implements OnInit, AfterViewInit {
+export class HubDetailsComponent implements AfterViewInit, OnDestroy, OnInit {
 
   /**
    * @summary data of the current hub
    */
   public hub: Hub;
+
+  /**
+   * @summary delay between each refresh in ms
+   */
+  private refreshInterval = 3_000;
+
+  /**
+   * @summary timeout for content refresh
+   */
+  private refreshTimeout;
 
   constructor(
     private hubService: HubService,
@@ -40,6 +52,18 @@ export class HubDetailsComponent implements OnInit, AfterViewInit {
       // Retrieve the current hub's data
       this.retrieveHubData();
     });
+
+    // Activate the refresh timer
+    this.refreshTimeout = setInterval(() =>
+      this.retrieveHubData(),
+      this.refreshInterval);
+  }
+
+  /**
+   * @summary clear refresh timeout
+   */
+  ngOnDestroy(): void {
+    clearInterval(this.refreshTimeout);
   }
 
   /**
@@ -57,10 +81,9 @@ export class HubDetailsComponent implements OnInit, AfterViewInit {
   private retrieveHubData(): void {
     this.hubService.getHub(this.hub.id)
       .subscribe(
-        (hubData: Hub) => {
-          this.hub = hubData;
-        },
-        (error: HttpErrorResponse) => {
+        (hubData: Hub) =>
+          this.hub = hubData,
+        (_: HttpErrorResponse) => {
           this.router.navigate([RouteName.BOARD]);
           this.toastr.error(
             'Impossible de récupérer les données du hub',
