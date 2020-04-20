c94437e0330d88823d36c68d5214949dd3312669
pBouillon <pro.pierre.bouillon@protonmail.com>
2020-04-20 11:53:56 +0200

    🌱 Create how does it works component

--- intechnet-moderator/src/app/homepage/global.module.ts
+++ intechnet-moderator/src/app/homepage/global.module.ts
@@ -4,12 +4,14 @@ import { RouterModule } from '@angular/router';
 import { HomepageComponent } from './homepage/homepage.component';
 import { ImageWidgetComponent } from './image-widget/image-widget.component';
 import { ContactComponent } from './contact/contact.component';
+import { HowDoesItWorksComponent } from './how-does-it-works/how-does-it-works.component';
 
 @NgModule({
   declarations: [
     HomepageComponent,
     ImageWidgetComponent,
     ContactComponent,
+    HowDoesItWorksComponent,
   ],
   imports: [
     CommonModule,
---
+++ intechnet-moderator/src/app/homepage/how-does-it-works/how-does-it-works.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-how-does-it-works',
+  templateUrl: './how-does-it-works.component.html',
+  styleUrls: ['./how-does-it-works.component.scss']
+})
+export class HowDoesItWorksComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
